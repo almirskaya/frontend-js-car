2a6af4f3be8a5c6bf4b9838d25e394ebd6050c36
almirskaya <alexamirskaya@gmail.com>
2016-10-05 00:40:20 +0300

    задание выполнено, тесты пройдены

--- src/js/car.js
+++ src/js/car.js
@@ -1,1 +1,43 @@
 ﻿'use strict';
+
+function Car(){
+    this.color='white';
+    this.dours=4;
+    this.speed=0;
+    this.defaultSpeed=60;
+    this.maxSpeed=100;
+    this.seats=4;
+    this.passengers=[];
+    this.put=function(){
+        if (this.passengers.length<this.seats){
+            this.passengers.push(true);
+        }
+        else if(this.passengers.length>this.seats){
+            this.passengers.pop();
+        }
+    };
+
+    this.land=function(){
+        this.passengers.pop();
+    };
+
+    this.drive=function (newSpeed){
+        if (this.passengers.length<1){
+            this.speed=0;
+        }
+        else if(this.passengers.length>this.seats){
+            this.speed=0;
+        }
+        else if (newSpeed>this.maxSpeed){
+            this.speed=this.maxSpeed;
+        }
+        else if (typeof newSpeed!=='number'){
+            this.speed=this.defaultSpeed;
+        }
+        else {
+            this.speed=newSpeed;
+        }
+    }  
+};
+var car=new Car();
+module.exports=car;
